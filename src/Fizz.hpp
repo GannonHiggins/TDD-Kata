1a0eafd87de94209c1c60c9835199cd781d986f0
Gannon Higgins <akg98@me.com>
2021-02-03 09:42:43 -0900

    modified tests and function

--- src/Fizz.hpp
+++ src/Fizz.hpp
@@ -1,8 +1,25 @@
 #ifndef FIZZ_HPP
 #define FIZZ_HPP
 
+#include <string>
 
-int FizzBuzz(int number){
+
+std::string FizzBuzz(std::string number){
+int buffer;
+
+std::string buf;
+buffer = std::stoi(number);
+
+if (buffer == 3){
+      buf = "Fizz";
+   return buf;
+}
+
+if(buffer == 5){
+
+   buf = "Buzz";
+   return buf;
+}
 
 
    return number; 
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,10 +1,16 @@
 #include "catch.hpp"
 #include "Fizz.hpp"
+#include <vector>
 
 
-
-TEST_CASE( "Empty string returns 0"){
-    REQUIRE( FizzBuzz(0) == 0 );
-    REQUIRE( FizzBuzz(1) == 0 );
+TEST_CASE( "using a string to input numbers"){
+    REQUIRE( FizzBuzz("0") == "0" );
+    REQUIRE( FizzBuzz("1") == "1" );
+    REQUIRE( FizzBuzz("2") == "2" );
+    REQUIRE( FizzBuzz("3") == "Fizz" );
+    REQUIRE( FizzBuzz("4") == "4" );
+    REQUIRE( FizzBuzz("5") == "Buzz" );
 }
+
+
 
